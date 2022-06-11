8276987dfb3dc353f49f4cdf5aec5d655bf3b686
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-11 08:26:16 -0300

    Initial commit

---
+++ src/main/java/br/dev/multicode/mcassessments/McAssessmentsApplication.java
@@ -0,0 +1,13 @@
+package br.dev.multicode.mcassessments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McAssessmentsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(McAssessmentsApplication.class, args);
+  }
+
+}
