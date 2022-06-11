cbd1bb4c48808adcf3533c9868b0fa88e18505a9
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-11 09:55:49 -0300

    Adicionada entidade

---
+++ src/main/java/br/dev/multicode/mcassessments/entities/Assessment.java
@@ -0,0 +1,43 @@
+package br.dev.multicode.mcassessments.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.ZonedDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "assessments")
+public class Assessment {
+
+  @Id
+  @Column(length = 37, nullable = false)
+  private String productId;
+
+  @Column(length = 37, nullable = false)
+  private String userId;
+
+  @Column(nullable = false)
+  private Integer assessment;
+
+  @Column(nullable = false)
+  private String description;
+
+  @CreationTimestamp
+  private ZonedDateTime createdAt;
+
+  @UpdateTimestamp
+  private ZonedDateTime updatedAt;
+
+}
