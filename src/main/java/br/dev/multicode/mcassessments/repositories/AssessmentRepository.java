c48de4545390c7c98edb0e89d4b70e79c4fa9455
Andre Ferreira <andre.dev.linux@gmail.com>
2022-06-11 10:37:34 -0300

    Adicionada consultas personalizadas

--- src/main/java/br/dev/multicode/mcassessments/repositories/AssessmentRepository.java
+++ src/main/java/br/dev/multicode/mcassessments/repositories/AssessmentRepository.java
@@ -4,6 +4,13 @@ import br.dev.multicode.mcassessments.entities.Assessment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface AssessmentRepository extends JpaRepository<Assessment, String> {
+
+  Set<Assessment> findAssessmentByUserId(String userId);
+
+  Set<Assessment> findAssessmentByProductId(String productId);
+
 }
